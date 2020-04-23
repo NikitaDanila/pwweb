4b0d69c93b8c363592bb042b89233f7290a7a7ab
Nikita Danila <danila.nikitamihai@gmail.com>
2020-04-23 23:44:40 +0300

    still working on it

--- includes/user.inc.php
+++ includes/user.inc.php
@@ -1,16 +1,37 @@
 <?php
 	session_start();
 	$user = $_SESSION["user"];
+	$won = $_SESSION["won"];
 	//echo $user;
 
 	class User{
+		var $userId;
 		var $bestRun;
 		var $newBest;
 		var $timesWon;
 
 		public function __construct(){
+			$this->userId = $user;
 			$this->bestRun = 0;
-			$thid->timesWon = 0;
+			$this->timesWon = 0;
+		}
+
+		// public function _construct($user){
+		// 	$this->userId = $user;
+		// 	$this->bestRun = 0;
+		// 	$this->timesWon = 0;
+		// }
+
+		public function _getId(){
+			return $this->userId;
+		}
+
+		public function _getBestRun(){
+			return $this->bestRun;
+		}
+
+		public function _getTimesWon(){
+			return $this->timesWon;
 		}
 
 		public function _bestRun($newBest){
@@ -18,7 +39,14 @@
 				$this->bestRun = $newBest;
 			}
 		}
-		public function _timesWon($_SESSION["won"]){
-			$this->timesWon = $_SESSION["won"];
+
+		public function _timesWon($won){
+			$this->timesWon = $won;
 		}
+	}
+
+	function _autocall(){
+		$userList = array();
+		$userList[] = new User($user);
+		echo $userList[0]->_getId();
 	}
